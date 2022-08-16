c120ef41d87f816e626c6e5cd9c1e8835ed348d9
nidup1010 <nidup1998@gmail.com>
2022-08-16 15:59:54 +0530

    fix database

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
 // app.use(express.static("client/build")) using relative path
 
 
